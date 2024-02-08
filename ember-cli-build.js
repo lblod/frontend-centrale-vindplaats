3dfba6660a751cee50de843dcffee175c57be688
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-02-08 14:36:09 +0100

    Update to Appuniversum v1
    This updates to v1.10.0 release which was the last v1 release. We can then start fixing deprecations and update to v2.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,14 +3,7 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {
-    sassOptions: {
-      includePaths: [
-        'node_modules/@appuniversum/appuniversum',
-        'node_modules/@appuniversum/ember-appuniversum/app/styles',
-      ],
-    },
-  });
+  let app = new EmberApp(defaults, {});
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
